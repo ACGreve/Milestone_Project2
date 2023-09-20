5fdf7dd2203f2f0c1da55afab25d9e575af49dde
Conor Panuccio <conorpanu@hotmail.com>
2023-09-20 18:16:31 -0400

    made actors loopable

--- controllers/actor.js
+++ controllers/actor.js
@@ -1,13 +1,21 @@
 const router = require('express').Router()
 const Actor = require('../models/actor')
 const actorSeedData = require('../models/actorSeedData')
+const { actorSeed } = require('../models/actorSeedData')
 
 //Seed data
 router.get('/data/seed', async(req, res) =>{
-    await Promise.all([Actor.deleteMany()])
-    const actors = await Actor.insertMany(actorSeedData)
-    const actorIds = actors.map(actor => actor._id)
-    res.redirect('/actors')
+    // await Promise.all([Actor.deleteMany()])
+    // const actors = await Actor.insertMany(actorSeedData)
+    // const actorIds = actors.map(actor => actor._id)
+    // res.redirect('/actors')
+    try{
+        await actorSeed()
+        res.json({ message: 'complete' })
+    }catch(error){
+        console.log('error', error)
+        res.status(500).json({ message: 'error saving actors' })
+    }
 })
 
 //home route for all actors
